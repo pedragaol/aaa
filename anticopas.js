1a4df176aa04663bca267d4b6aae0e124bf1645f
pedragaol <38577850+pedragaol@users.noreply.github.com>
2018-04-21 11:39:15 +0700

    Create anticopas.js

---
+++ anticopas.js
@@ -0,0 +1,8 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type='text/javascript'>//<![CDATA[ 
+ $(function() {
+        $(this).bind("contextmenu", function(e) {
+            e.preventDefault();
+        });
+    }); //]]>  
+</script>
